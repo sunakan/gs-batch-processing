8e22dce8bc3554b965ad6280c88788b415a42d03
sunakan <sunakan03@gmail.com>
2021-01-08 21:39:01 +0900

    table初期化状態の確認から先にする

--- src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
+++ src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
@@ -1,13 +1,25 @@
 package com.example.batchprocessing;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class BatchProcessingApplication {
+public class BatchProcessingApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BatchProcessingApplication.class, args);
     }
 
+    @Autowired
+    JdbcTemplate j;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("----------------------------------------------------------------");
+        System.out.println(j.queryForObject("select count(*) from people", Integer.class));
+        System.out.println("----------------------------------------------------------------");
+    }
 }
